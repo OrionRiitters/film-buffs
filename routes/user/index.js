9d3caf32fed6dca2980a607a93a64b291f9535fb
Orion McCoy-Riitters <orionriitters@gmail.com>
2019-05-15 07:23:29 -0500

    Fixed issue with dotenv

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,16 +1,5 @@
 const router = require('express').Router()
 const Sequelize = require('sequelize')
-const dotenv = require('dotenv')
-
-dotenv.config()
-
-const sequelize = new Sequelize(process.env.POSTGRES_URI, {
-  dialect: 'postgres',
-})
-sequelize
-  .authenticate()
-  .then(() => console.log('connected to postgres'))
-  .catch(err => console.log('error connecting', err))
 
 module.exports = function(User) {
   router.post('/', function(req, res, next) {
